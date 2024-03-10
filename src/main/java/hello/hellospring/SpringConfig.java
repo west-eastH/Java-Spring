6cfd00b157d29a71db03f7bbae9fef65831d0c6b
donghyeon <west.east1832@gmail.com>
2024-03-10 15:51:43 +0900

    [feat] : AOP

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,33 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
 
-	@Bean
-	public MemberService memberService() {
-		return new MemberService(memberRepository());
+	private final MemberRepository memberRepository;
+
+	public SpringConfig(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
 	}
 
 	@Bean
-	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+	public MemberService memberService() {
+		return new MemberService(memberRepository);
 	}
+
+
+//	@Bean
+//	public MemberRepository memberRepository() {
+//		return new MemoryMemberRepository();
+//		return new JdbcMemberRepository(dataSource);
+//		return new JdbcTemplateMemberRepository(dataSource);
+//		return new JpaMemberRepository(em);
+//	}
 }
